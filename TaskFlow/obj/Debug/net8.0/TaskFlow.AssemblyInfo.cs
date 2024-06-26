ea6cf8a478ad74aa7c52ce0d825585f2e30e0ca7
JacobsenNando <fernandofjlr@gmail.com>
2024-06-26 20:47:55 -0300

    .

--- TaskFlow/obj/Debug/net8.0/TaskFlow.AssemblyInfo.cs
+++ TaskFlow/obj/Debug/net8.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d5cf4a5d8981e346a3d7743b0acfb7c16adbfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f45eb5875a8d944a372534861e91d9bb264f87a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
